89784fa926eb501e0efd576f5006b8b356512d02
Sebastien Cadorette <sebastien.cadorette@logibec.com>
2019-08-12 13:53:53 -0400

    identifiers

--- src/bd.ts
+++ src/bd.ts
@@ -5,7 +5,7 @@ import { Mongoose } from 'mongoose';
 @injectable()
 export class BD {
     // private readonly uri: string = 'mongodb://admin:admin@localhost:27017/chat-test';
-    private readonly uri: string = 'mongodb+srv://user1:allo@chat-test-zpcun.mongodb.net/chat-test?retryWrites=true&w=majority';
+    private readonly uri: string = 'mongodb+srv://admin:admin@chat-test-zpcun.mongodb.net/chat-test?retryWrites=true&w=majority';
     private mongoose: Mongoose;
 
     public constructor() {
