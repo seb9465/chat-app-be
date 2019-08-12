53435cdd2bd72824943ff34512f928ef8616c746
Sebastien Cadorette <sebastien.cadorette@logibec.com>
2019-08-12 13:51:11 -0400

    mongoose usage

--- src/bd.ts
+++ src/bd.ts
@@ -1,28 +1,25 @@
 import { injectable } from "inversify";
-import { MongoClient, Collection } from "mongodb";
 import { Response } from "express";
+import { Mongoose } from 'mongoose';
 
 @injectable()
 export class BD {
     // private readonly uri: string = 'mongodb://admin:admin@localhost:27017/chat-test';
-    private readonly uri: string = 'mongodb://admin:admin@chat-test-zpcun.mongodb.net/test';
-    private client: MongoClient;
+    private readonly uri: string = 'mongodb+srv://user1:allo@chat-test-zpcun.mongodb.net/chat-test?retryWrites=true&w=majority';
+    private mongoose: Mongoose;
 
     public constructor() {
-        this.client = new MongoClient(this.uri, { useNewUrlParser: true });
+        this.mongoose = new Mongoose();
     }
 
     public connectToDb(res: Response): void {
-        this.client.connect(err => {
+        this.mongoose.connect(this.uri, { useNewUrlParser: true }, (err) => {
             if (err) {
-                console.log('ERROR');
                 console.log(err);
                 res.send(err);
             } else {
-                const collection: Collection = this.client.db('chat-test').collection('users');
-                console.log('connected');
-                console.log(collection);
-                res.send("Working");
+                console.log("WORKING");
+                res.send("WORKING");
             }
         });
     }
